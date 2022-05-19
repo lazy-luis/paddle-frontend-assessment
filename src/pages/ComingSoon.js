d2285df13df1d6e4cb5f167c2a7f796c060ec77f
Adesiyan Divine <divinegiftadesiyan@gmail.com>
2022-05-20 00:26:38 +0100

    Coming Soon Page Design

--- src/pages/ComingSoon.js
+++ src/pages/ComingSoon.js
@@ -1,7 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
 
 const ComingSoon = () => {
-  return <div>ComingSoon</div>;
+  const [firstName, firstNameState] = useState("");
+  const [lastName, lastNameState] = useState("");
+  const [email, emailState] = useState("");
+  return (
+    <>
+      <Header />
+      <div className="ComingSoon">
+        <h1> SOMETHING AWESOME IS COMING SOON </h1>
+        <h3>
+          Your all-in-one affiliate marketing tracking software
+          <span> track, automate</span> and <span>optimize</span> your campaigns
+        </h3>
+        <div className="CountdownTimer">
+          <div className="CountdownBlock">
+            <p> 7 </p>
+            <label> Days </label>
+          </div>
+          <div className="CountdownBlock">
+            <p> 24 </p>
+            <label> Hours </label>
+          </div>
+          <div className="CountdownBlock">
+            <p> 54 </p>
+            <label> Minutes </label>
+          </div>
+          <div className="CountdownBlock">
+            <p> 11 </p>
+            <label> Seconds </label>
+          </div>
+        </div>
+        <div className="WaitingList">
+          <div className="WaitingListInputs">
+            <input
+              type={"text"}
+              placeholder={"First Name..."}
+              value={firstName}
+              onInput={(e) => firstNameState(e.target.value)}
+            />
+            <input
+              type={"text"}
+              placeholder={"Last Name..."}
+              value={lastName}
+              onInput={(e) => lastNameState(e.target.value)}
+            />
+          </div>
+          <div className="WaitingListMailBtn">
+            <input
+              type={"email"}
+              placeholder={"Enter your email address..."}
+              value={email}
+              onInput={(e) => emailState(e.target.value)}
+            />
+            <button> JOIN OUR WAITING LIST </button>
+          </div>
+        </div>
+      </div>
+      <Footer ComingSoon={true} />
+    </>
+  );
 };
 
 export default ComingSoon;
