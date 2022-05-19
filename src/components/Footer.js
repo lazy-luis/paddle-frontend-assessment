1c4e19a2856c4a8e27fe2ce33929e2dadd6d23be
Adesiyan Divine <divinegiftadesiyan@gmail.com>
2022-05-20 00:26:25 +0100

    Footer Inclusion

---
+++ src/components/Footer.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { BsYoutube } from "react-icons/bs";
+import { FaFacebookSquare, FaLinkedin, FaTwitterSquare } from "react-icons/fa";
+import { RiInstagramFill } from "react-icons/ri";
+import { Link } from "react-router-dom";
+
+const Footer = ({ ComingSoon = false }) => {
+  return (
+    <footer className={ComingSoon ? "ComingSoonFooter" : ""}>
+      <div className="FooterSocials">
+        <BsYoutube />
+        <FaFacebookSquare />
+        <FaLinkedin />
+        <RiInstagramFill />
+        <FaTwitterSquare />
+      </div>
+      <div className="FooterLegal">
+        <Link to={"#"}> Terms of services </Link>
+        <Link to={"#"}> Privacy policy </Link>
+      </div>
+      <div className="FooterCopy">
+        <p> Copyright 2021 @ Peddle Technologies. All Rights Reserved. </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
